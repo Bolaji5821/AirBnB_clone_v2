418635bc9b12cbbebdf31112a2b539c801bebc4c
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-10-15 01:02:15 -0700

    display hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A script that starts a flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Displays a string when the route / is requested
+    """
+    return "Hello HBNB"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Displays hbnb
+    """
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
