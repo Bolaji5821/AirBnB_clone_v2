522b0be7f57fcca16d7a9f08fb2e32b7af0146b2
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-10-19 18:24:15 -0700

    States

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
-'''crates a flask web application'''
-from flask import Flask, render_template
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /cities_by_states: HTML page with a list of all states and related cities.
+"""
 from models import storage
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-app.jinja_env.lstrip_blocks = True
 
-
-@app.route('/states_list')
-def display_states():
-    return render_template('7-states_list.html', storage=storage.all('State'))
-
-
-@app.route('/cities_by_states')
-def display_cities_by_states():
-    return render_template('8-cities_by_states.html',
-                           storage=storage.all('State'))
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Displays an HTML page with a list of all states and related cities.
+    States/cities are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
-def close(exception):
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
